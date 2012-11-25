9bb53967b07226c8c94785d42fd2c85155545e5c
Louis Mullie <louis.mullie@gmail.com>
2012-11-24 19:14:58 -0500

    Remove NLTK dependency to support Amazon EMR.

--- wordFrequency.py
+++ wordFrequency.py
@@ -1,6 +1,6 @@
 from mrjob.job import MRJob
 from mrjob.protocol import JSONProtocol
-import nltk
+import re
 
 class wordFrequency(MRJob):
  
@@ -28,15 +28,21 @@ class wordFrequency(MRJob):
 	 'under', 'therefore', 'nonetheless', 'thereafter',
 	 'afterwards', 'here', 'huh', 'hah', "n't", "'t", 'here',
 	 'neither', 'towards']
-
+   
  def mapper(self, doc, sentences):
    for i, sentence in enumerate(sentences):
-     words = nltk.word_tokenize(sentence)
+     words = self.tokenize(sentence)
      for j, word in enumerate(words):
        if word in self.stop: continue
        k = (word, doc, str(i))
        try: self.freqs[k] += 1
        except: self.freqs[k] = 1
+
+ def tokenize(self, txt):
+   p = re.compile(r"\d+|[-'a-z]+|[ ]+|\s+|[.,]+|\S+", re.I)
+   slice_starts = [m.start() for m in p.finditer(txt)] + [None]
+   r = [txt[s:e] for s, e in zip(slice_starts, slice_starts[1:])]
+   return r
 
  def mapper_final(self):
    for key in self.freqs.iterkeys():
